3b724578b49b5139c3627606b5b8a4377f80dcf0
RemiBLoizeau <remi.b.loizeau@gmail.com>
2023-05-13 18:38:41 +0200

    Patch 0.1.086__v004

--- core/fonctions.php
+++ core/fonctions.php
@@ -3453,13 +3453,16 @@ function nadine_prix($prix)
 /**
  * La fonction nadine_log(), surnommée TheMadManagerMode™
  * par le service marketing de NadineCorp.©, permet aux équipes
- * du NadineCrew et NadineLab© de déboguer Nadine plus efficacement
+ * du NadineCrew et du NadineLab© de déboguer Nadine plus efficacement
  * en espionnant ses moindres faits et gestes.
  */
 
 function nadine_log($msg)
 {
-  if (isset($msg)) {
+  // Permet d'activer ou désactiver le MadManagerMode™
+  $MadManagerMode = false;
+
+  if (isset($msg) && $MadManagerMode == true) {
     $error_msg = date('Y-m-d H:i:s') . ' - ' . __FILE__ . "\n";
     $error_msg .= $msg . "\n";
     error_log($error_msg . PHP_EOL, 3, 'nadine__journal.log');
